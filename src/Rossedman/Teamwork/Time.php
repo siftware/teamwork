8d807dc51c4605d47d18bf9058c35a61a7d7b1a9
Adam Griffiths <adam.griffiths@siftware.com>
2016-03-18 15:32:13 +0000

    Update Time.php to allow us to use updatedAfterDate parameter

--- src/Rossedman/Teamwork/Time.php
+++ src/Rossedman/Teamwork/Time.php
@@ -17,7 +17,7 @@ class Time extends AbstractObject {
      */
     public function all($args = null)
     {
-        $this->areArgumentsValid($args, ['page']);
+        $this->areArgumentsValid($args, ['page', 'updatedAfterDate']);
 
         return $this->client->get($this->endpoint, $args)->response();
     }
